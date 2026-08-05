40f887f051cef630ace0b1973d3d09aa2a26dbad
Jakub Wartak <jakub.wartak@enterprisedb.com>
2026-08-05 08:53:31 +0200

    pg_basebackup: add support for Direct I/O and Async I/O using liburing
    Profiling pg_basebackup when doing high-data transfer reveals that it is
    highly CPU-bound on kernel side inside write() due to kernel's side
    page-cache memory allocations/cgroups memory accounting and copying the data
    from userspace back to kernel side, just to be copied again to the device.
    
    To avoid that we can do Direct Memory Transfers from our userspace buffers
    directly to the storage device, however we cannot do use synchronous API
    for that due to high individual device latency. On Linux however, we are
    already using liburing for the backend code, so this commit teaches
    pg_basebackup/astreamers (for writing plain and tar formats) to write using
    AIO+DIO with deep I/O queues. It realistic benchmarks and with adequate
    sequential storage bandwidth and no SSL encryption this makes possible for
    pg_basebackup -Fp to go from ~1610MB/s to ~2975MB/s as long as the data is
    hot in page-cache of the source server and the network and single stream TCP
    socket performance is able to keep up.
    
    This commits also exposes undocumented/debugging PG_BASEBACKUP_NODIO env
    variable, that when set allows disabling this optimization.
    
    Author: Jakub Wartak <jakub.wartak@enterprisedb.com>

--- src/bin/pg_basebackup/pg_basebackup.c
+++ src/bin/pg_basebackup/pg_basebackup.c
@@ -1153,7 +1153,8 @@ CreateBackupStreamer(char *archive_name, char *spclocation,
 		streamer = astreamer_extractor_new(directory,
 										   get_tablespace_mapping,
 										   progress_update_filename,
-										   backup_target_clientblackhole);
+										   backup_target_clientblackhole,
+										   verbose);
 	}
 	else
 	{
@@ -1191,7 +1192,7 @@ CreateBackupStreamer(char *archive_name, char *spclocation,
 		 */
 		if (compress->algorithm == PG_COMPRESSION_NONE)
 			streamer = astreamer_plain_writer_new(archive_filename,
-												  archive_file);
+												  archive_file, verbose);
 		else if (compress->algorithm == PG_COMPRESSION_GZIP)
 		{
 			if (!backup_target_clientblackhole)
@@ -1204,7 +1205,7 @@ CreateBackupStreamer(char *archive_name, char *spclocation,
 			if (!backup_target_clientblackhole)
 				strlcat(archive_filename, ".lz4", sizeof(archive_filename));
 			streamer = astreamer_plain_writer_new(archive_filename,
-												  archive_file);
+												  archive_file, verbose);
 			streamer = astreamer_lz4_compressor_new(streamer, compress);
 		}
 		else if (compress->algorithm == PG_COMPRESSION_ZSTD)
@@ -1212,7 +1213,7 @@ CreateBackupStreamer(char *archive_name, char *spclocation,
 			if (!backup_target_clientblackhole)
 				strlcat(archive_filename, ".zst", sizeof(archive_filename));
 			streamer = astreamer_plain_writer_new(archive_filename,
-												  archive_file);
+												  archive_file, verbose);
 			streamer = astreamer_zstd_compressor_new(streamer, compress);
 		}
 		else
