7a7bc7f80e9b280eb46e4dd14afd1f7c6551a142
okbob@github.com <okbob@github.com>
2026-08-12 07:03:28 +0200

    introduce 'tam' modifier for date/timestamp formatting
    glibc returns localized month names in the genitive case, but it also
    provides alternative month names in the nominative case. The TAM prefix
    allows you to retrieve this alternative name.
    
    example:
    
    SELECT to_char(date '2026-08-01', 'TMMONTH');
    ┌─────────┐
    │ to_char │
    ╞═════════╡
    │ SRPNA   │
    └─────────┘
    (1 row)
    
    SELECT to_char(date '2026-08-01', 'TAMMONTH');
    ┌─────────┐
    │ to_char │
    ╞═════════╡
    │ SRPEN   │
    └─────────┘
    (1 row)

--- src/include/utils/pg_locale.h
+++ src/include/utils/pg_locale.h
@@ -59,7 +59,9 @@ extern PGDLLIMPORT int icu_validation_level;
 extern PGDLLIMPORT char *localized_abbrev_days[];
 extern PGDLLIMPORT char *localized_full_days[];
 extern PGDLLIMPORT char *localized_abbrev_months[];
+extern PGDLLIMPORT char *localized_alt_abbrev_months[];
 extern PGDLLIMPORT char *localized_full_months[];
+extern PGDLLIMPORT char *localized_alt_full_months[];
 
 extern bool check_locale(int category, const char *locale, char **canonname);
 extern char *pg_perm_setlocale(int category, const char *locale);
