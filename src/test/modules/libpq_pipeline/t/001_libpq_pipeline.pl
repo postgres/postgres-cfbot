bonnefoa/postgres

Add trace of startup packets in libpq_pipeline tests
The current way to enable tracing in libpq_pipeline only set PQtrace
after the connection is created, missing the trace of the startup
packets.

This patch replaces it by the trace_file connection parameter, enabling
PQtrace before the first packet is sent and thus tracing all protocol
messages.

src/test/modules/libpq_pipeline/t/001_libpq_pipeline.pl
@@ -44,9 +44,16 @@ for my $testname (@tests)
 	# For a bunch of tests, generate a libpq trace file too.
 	my $traceout =
 	  "$PostgreSQL::Test::Utils::tmp_check/traces/$testname.trace";
+
+	local $ENV{PGTRACE};
+	local $ENV{PGTRACEFLAGS};
 	if ($cmptrace)
 	{
-		push @extraargs, "-t" => $traceout;
+		$ENV{PGTRACE} = $traceout;
+		# Set trace flags to PQTRACE_SUPPRESS_TIMESTAMPS | PQTRACE_REGRESS_MODE
+		$ENV{PGTRACEFLAGS} = 3;
+	} else {
+		delete $ENV{PGTRACE};
 	}
 
 	# Execute the test using the latest protocol version.

postgres/postgres-cfbot

src/test/modules/libpq_pipeline/t/001_libpq_pipeline.pl
@@ -44,9 +44,16 @@ for my $testname (@tests)
 	# For a bunch of tests, generate a libpq trace file too.
 	my $traceout =
 	  "$PostgreSQL::Test::Utils::tmp_check/traces/$testname.trace";
+
+	local $ENV{PQTRACE};
+	local $ENV{PQTRACEFLAGS};
 	if ($cmptrace)
 	{
-		push @extraargs, "-t" => $traceout;
+		$ENV{PQTRACE} = $traceout;
+		# Set trace flags to PQTRACE_SUPPRESS_TIMESTAMPS | PQTRACE_REGRESS_MODE
+		$ENV{PQTRACEFLAGS} = 3;
+	} else {
+		delete $ENV{PQTRACE};
 	}
 
 	# Execute the test using the latest protocol version.