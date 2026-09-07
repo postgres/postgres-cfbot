hackorum-dev/postgres

Support EXCEPT for ALL SEQUENCES in CREATE PUBLICATION
Extend CREATE PUBLICATION ... FOR ALL SEQUENCES to support the
EXCEPT syntax. This allows one or more sequences to be excluded. The
publisher will not send the data of excluded sequences to the subscriber.

Example:
CREATE PUBLICATION pub1 FOR ALL SEQUENCES EXCEPT (SEQUENCE s1, s2);

src/include/nodes/parsenodes.h
@@ -4465,14 +4465,14 @@ typedef struct AlterTSConfigurationStmt
 	bool		missing_ok;		/* for DROP - skip error if missing? */
 } AlterTSConfigurationStmt;
 
-typedef struct PublicationTable
+typedef struct PublicationRelation
 {
 	NodeTag		type;
 	RangeVar   *relation;		/* publication relation */
 	Node	   *whereClause;	/* qualifications */
 	List	   *columns;		/* List of columns in a publication table */
 	bool		except;			/* True if listed in the EXCEPT clause */
-} PublicationTable;
+} PublicationRelation;
 
 /*
  * Publication object type
@@ -4481,6 +4481,7 @@ typedef enum PublicationObjSpecType
 {
 	PUBLICATIONOBJ_TABLE,		/* A table */
 	PUBLICATIONOBJ_EXCEPT_TABLE,	/* A table in the EXCEPT clause */
+	PUBLICATIONOBJ_EXCEPT_SEQUENCE, /* A sequence in the EXCEPT clause */
 	PUBLICATIONOBJ_TABLES_IN_SCHEMA,	/* All tables in schema */
 	PUBLICATIONOBJ_TABLES_IN_CUR_SCHEMA,	/* All tables in first element of
 											 * search_path */
@@ -4492,7 +4493,7 @@ typedef struct PublicationObjSpec
 	NodeTag		type;
 	PublicationObjSpecType pubobjtype;	/* type of this publication object */
 	char	   *name;
-	PublicationTable *pubtable;
+	PublicationRelation *pubrelation;
 	ParseLoc	location;		/* token location, or -1 if unknown */
 } PublicationObjSpec;
 
@@ -4509,7 +4510,9 @@ typedef struct PublicationAllObjSpec
 {
 	NodeTag		type;
 	PublicationAllObjType pubobjtype;	/* type of this publication object */
-	List	   *except_tables;	/* tables specified in the EXCEPT clause */
+	List	   *except_relations;	/* depending on the 'pubobjtype', this is
+									 * a list of either tables or sequences
+									 * specified in the EXCEPT clause */
 	ParseLoc	location;		/* token location, or -1 if unknown */
 } PublicationAllObjSpec;
 

postgres/postgres-cfbot

src/include/nodes/parsenodes.h
@@ -4475,14 +4475,16 @@ typedef struct AlterTSConfigurationStmt
 	bool		missing_ok;		/* for DROP - skip error if missing? */
 } AlterTSConfigurationStmt;
 
-typedef struct PublicationTable
+typedef struct PublicationRelation
 {
 	NodeTag		type;
-	RangeVar   *relation;		/* publication relation */
-	Node	   *whereClause;	/* qualifications */
-	List	   *columns;		/* List of columns in a publication table */
-	bool		except;			/* True if listed in the EXCEPT clause */
-} PublicationTable;
+	RangeVar   *relation;		/* publication table/sequence */
+	Node	   *whereClause;	/* qualifications for publication table; NULL
+								 * for sequence */
+	List	   *columns;		/* List of columns in a publication table;
+								 * NULL for sequence */
+	bool		except;			/* True if listed in an EXCEPT clause */
+} PublicationRelation;
 
 /*
  * Publication object type
@@ -4490,7 +4492,8 @@ typedef struct PublicationTable
 typedef enum PublicationObjSpecType
 {
 	PUBLICATIONOBJ_TABLE,		/* A table */
-	PUBLICATIONOBJ_EXCEPT_TABLE,	/* A table in the EXCEPT clause */
+	PUBLICATIONOBJ_EXCEPT_TABLE,	/* A table in an EXCEPT clause */
+	PUBLICATIONOBJ_EXCEPT_SEQUENCE, /* A sequence in an EXCEPT clause */
 	PUBLICATIONOBJ_TABLES_IN_SCHEMA,	/* All tables in schema */
 	PUBLICATIONOBJ_TABLES_IN_CUR_SCHEMA,	/* All tables in first element of
 											 * search_path */
@@ -4502,7 +4505,7 @@ typedef struct PublicationObjSpec
 	NodeTag		type;
 	PublicationObjSpecType pubobjtype;	/* type of this publication object */
 	char	   *name;
-	PublicationTable *pubtable;
+	PublicationRelation *pubrelation;
 	ParseLoc	location;		/* token location, or -1 if unknown */
 } PublicationObjSpec;
 
@@ -4519,7 +4522,9 @@ typedef struct PublicationAllObjSpec
 {
 	NodeTag		type;
 	PublicationAllObjType pubobjtype;	/* type of this publication object */
-	List	   *except_tables;	/* tables specified in the EXCEPT clause */
+	List	   *except_relations;	/* depending on the 'pubobjtype', this is
+									 * a list of either tables or sequences
+									 * specified in an EXCEPT clause */
 	ParseLoc	location;		/* token location, or -1 if unknown */
 } PublicationAllObjSpec;
 