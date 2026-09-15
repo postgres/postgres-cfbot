825d9b6c7802e5ddf091855c4e19aa9e602416b5
ChenHui Mo <chenhuimo.mch@qq.com>
2026-09-15 15:51:53 +0800

    Return early from repeat() for empty results
    Avoid entering the copy loop when the repeat count is zero or the source
    string is empty.

--- src/test/regress/sql/strings.sql
+++ src/test/regress/sql/strings.sql
@@ -1179,6 +1179,8 @@ SELECT chr(0);
 
 SELECT repeat('Pg', 4);
 SELECT repeat('Pg', -4);
+SELECT repeat('', 10);
+SELECT repeat('x', 0);
 
 SELECT SUBSTRING('1234567890'::bytea FROM 3) "34567890";
 SELECT SUBSTRING('1234567890'::bytea FROM 4 FOR 3) AS "456";
