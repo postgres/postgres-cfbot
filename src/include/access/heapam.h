0b2e8437b878bb0adc03e440b06eda8483a4cf20
Antonin Houska <ah@cybertec.at>
2026-09-16 18:47:27 +0200

    Make REPACK (CONCURRENTLY) MVCC-safe.
    In the initial implementation of REPACK (CONCURRENTLY), tuples in the new
    table are marked with XID of the transaction the command executes in. This is
    simpler to implement, but can cause surprising behavior: on specific
    conditions, the table contents may become invisible for transactions that
    should see it. (Both ALTER TABLE and TRUNCATE commands have this problem.)
    Besides that, as REPACK (CONCURRENTLY) needs to have XID assigned (to mark the
    new tuples) while it copies the data (which can take long time), it can
    restrict the progress of the xmin horizons for VACUUM quite a bit.
    
    This patch teaches REPACK (CONCURRENTLY) to transfer the visibility
    information from the old table to the new one. Thus there's no need to have
    XID assigned during the copying, and therefore VACUUM (of other tables) is not
    restricted anymore.
    
    A new type of snapshot is used to look for the existing tuples in the new
    table, in order to apply the concurrent data changes (i.e. changes performed
    by other transactions during the initial copying). This snapshot assumes that
    the new table does not contain any tuples created by aborted
    transactions. This is simpler and more efficient than building historic MVCC
    snapshots for the look-ups.
    
    One problem that needs special attention is tuple freezing. Besides page
    pruning code, freezing is currently implemented only in rewriteheap.c. Thus it
    seems simpler to teach REPACK (CONCURRENTLY) to copy the data using
    rewriteheap.c rather than implementing freezing of individual tuples
    independent from page pruning. (The latter would include WAL logging of the
    individual frozen tuples, whereas rewriteheap.c logs the whole page at once.)
    Disadvantage of this approach is that we only freeze tuples during the initial
    copying, while the data changes decoded from WAL are replayed without
    freezing.
    
    This patch also uses rewriteheap.c (in particular raw_heap_insert()) to insert
    TOAST tuples. That seems to be the easiest way to avoid getting XID assigned
    just to insert TOAST. However, due to not using a new XID, even TOAST needs to
    be frozen now - again, we use the existing code in rewriteheap.c.

--- src/include/access/heapam.h
+++ src/include/access/heapam.h
@@ -381,6 +381,7 @@ extern void heap_multi_insert(Relation relation, TupleTableSlot **slots,
 							  int ntuples, CommandId cid, uint32 options,
 							  BulkInsertState bistate);
 extern TM_Result heap_delete(Relation relation, const ItemPointerData *tid,
+							 TransactionId xid,
 							 CommandId cid, uint32 options, Snapshot crosscheck,
 							 bool wait, TM_FailureData *tmfd);
 extern void heap_finish_speculative(Relation relation, const ItemPointerData *tid);
@@ -423,7 +424,8 @@ extern bool heap_tuple_should_freeze(HeapTupleHeader tuple,
 extern bool heap_tuple_needs_eventual_freeze(HeapTupleHeader tuple);
 
 extern void simple_heap_insert(Relation relation, HeapTuple tup);
-extern void simple_heap_delete(Relation relation, const ItemPointerData *tid);
+extern void simple_heap_delete(Relation relation, const ItemPointerData *tid,
+							   TransactionId xid);
 extern void simple_heap_update(Relation relation, const ItemPointerData *otid,
 							   HeapTuple tup, TU_UpdateIndexes *update_indexes);
 
